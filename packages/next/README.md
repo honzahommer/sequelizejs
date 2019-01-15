3227c44e30f44f1ccb85b4ce0abb559eab69d79a
Honza Hommer <honza@hommer.cz>
2019-01-15 21:38:34 +0100

    Reinventing the wheel. Again.

---
+++ packages/next/README.md
@@ -0,0 +1,9 @@
+# sequelizejs-next
+> For those, who needs two different versions of Sequelize installed in one project (eg. for tests).
+
+```
+npm install sequelizejs-next sequelize
+...
+const sequelizeNext = require('sequelizejs-next');
+const sequelize = require('sequelize');
+```
---
+++ packages/next/test/index.test.js
@@ -0,0 +1,15 @@
+const expect = require('expect.js');
+
+describe('@sequelize/next', function () {
+  before(function () {
+    this.lib = require('../lib');
+  });
+
+  it('returns the package', function () {
+    expect(this.lib).to.be.ok();
+  });
+
+  it('returns the correct version', function () {
+    expect(parseInt(this.lib.version.split('.').shift())).to.be(5);
+  });
+});
